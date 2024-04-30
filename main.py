af037eb4a28fab876e65efcef22e2896082de10a
Lindon Matarong <lindon2817@gmail.com>
2024-04-30 21:54:23 +0800

    remove bg working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt
 from visionperfect_ui import Ui_MainWindow
-
+from PIL import Image
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -24,6 +24,7 @@ class MainWindow(QMainWindow):
         self.ui.btn_resultbg.clicked.connect(self.show_resultbg)
         self.ui.pushButton_15.clicked.connect(self.select_image)
         self.selected_file_path = None
+        self.ui.btn_downloadbg.clicked.connect(self.download_result_image)
 
         # Hide btn when the program starts
         self.ui.widget_4.hide()
@@ -43,13 +44,12 @@ class MainWindow(QMainWindow):
 
     def show_info_page(self):
         self.ui.stackedWidget.setCurrentIndex(2)
+    
     def show_originalbg(self):
         self.ui.stackedWidget_3.setCurrentIndex(2)
+
     def show_resultbg(self):
         self.ui.stackedWidget_3.setCurrentIndex(0)
-    
-
-        
 
     def select_image(self):
         file_dialog = QFileDialog()
@@ -58,50 +58,55 @@ class MainWindow(QMainWindow):
         if file_path:
             self.selected_file_path = file_path
             pixmap = QPixmap(file_path)
-            pixmap = pixmap.scaled(self.ui.lbl_imgbg_result.size(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
+            pixmap = pixmap.scaled(self.ui.lbl_imgbg_result.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
             self.ui.lbl_imgbg_result.setPixmap(pixmap)
             self.ui.lbl_imgbg_result.setScaledContents(True)
             self.ui.lbl_imgbg_result.adjustSize()
             self.ui.pushButton_15.hide()
             self.ui.btn_rbg.show()
-            self.ui.btn_origbg.show()
-            self.ui.btn_downloadbg.show()
-            self.ui.btn_resultbg.show()
             self.ui.btn_rbg.clicked.connect(self.remove_background)
-
-
-
-
-
-    def select_image(self):
-        file_dialog = QFileDialog()
-        file_path, _ = file_dialog.getOpenFileName(self, "Select Image", "", "Image Files (*.png *.jpg *.jpeg)")
-
-        if file_path:
-            pixmap = QPixmap(file_path)
-            pixmap = pixmap.scaled(self.ui.lbl_imgbg_result.size(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
-            self.ui.lbl_imgbg_result.setPixmap(pixmap)
-            self.ui.lbl_imgbg_result.setScaledContents(True)
-            self.ui.lbl_imgbg_result.adjustSize()
-            self.ui.pushButton_15.hide()
-            self.ui.btn_rbg.show()
-            self.ui.btn_origbg.show()
-            self.ui.btn_downloadbg.show()
-            self.ui.btn_resultbg.show()
-            self.ui.btn_rbg.clicked.connect(self.remove_background)
+            self.show_originalbg()
 
     def remove_background(self):
         if self.selected_file_path is None:
             return
 
         with open(self.selected_file_path, "rb") as f:
-            img = rembg.remove(f)
+            input_image = Image.open(f)
+            output_image = rembg.remove(input_image)
 
-        pixmap = QPixmap.fromImage(QImage(img.tobytes(), img.shape[1], img.shape[0], QImage.Format_RGBA8888))
-        pixmap = pixmap.scaled(self.ui.lbl_imgbg.size(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
-        self.ui.lbl_imgbg.setPixmap(pixmap)
+        # Create a new image with a transparent background
+        transparent_background = Image.new('RGBA', output_image.size, (0, 0, 0, 0))
+
+        # Paste the output image onto the transparent background
+        transparent_background.paste(output_image, mask=output_image)
+
+        # Convert the output image to a QPixmap
+        output_pixmap = QPixmap.fromImage(QImage(transparent_background.tobytes(), transparent_background.size[0], transparent_background.size[1], QImage.Format_RGBA8888))
+        output_pixmap = output_pixmap.scaled(self.ui.lbl_imgbg.size(), Qt.AspectRatioMode.IgnoreAspectRatio, Qt.TransformationMode.SmoothTransformation)
+
+        # Set the output image as the pixmap for lbl_imgbg
+        self.ui.lbl_imgbg.setPixmap(output_pixmap)
         self.ui.lbl_imgbg.setScaledContents(True)
         self.ui.lbl_imgbg.adjustSize()
+        self.ui.btn_downloadbg.show()
+        self.ui.btn_origbg.show()
+        self.ui.btn_resultbg.show()
+        self.show_resultbg()
+
+    def download_result_image(self):
+        file_dialog = QFileDialog()
+        file_name, _ = file_dialog.getSaveFileName(self, "Save Image", "result.png", "Image Files (*.png)")
+
+        if file_name:
+            # Convert the output image to a QImage
+            output_image = QImage(self.ui.lbl_imgbg.pixmap().toImage())
+
+            # Save the output image
+            output_image.save(file_name)
+
+            # Show a message box to confirm the save
+            QMessageBox.information(self, "Image Saved",f"Image saved as {file_name}")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
